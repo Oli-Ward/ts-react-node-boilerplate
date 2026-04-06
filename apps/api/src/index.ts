a9dc357d27619dfb5711ab88fd3d7fabd201276d
Oliver Ward <oliver.ward94@gmail.com>
2026-04-06 15:22:33 +1200

    chore: add cors & dotenv to index

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,11 @@
 import { createApp } from "./app";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = createApp();
-
-app.listen(3000, () => {
+app.use(cors());
+app.listen(process.env.API_PORT, () => {
     console.log("Server running");
 });
