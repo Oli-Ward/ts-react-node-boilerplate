d90a894f293eb3e93828fb7d72c6e32a165ce984
Oliver Ward <oliver.ward94@gmail.com>
2026-04-07 12:35:14 +1200

    feat: add frontend vitest and hello world frontend

--- apps/web/src/tests/App.test.tsx
+++ apps/web/src/tests/App.test.tsx
@@ -1,1 +1,12 @@
-// To do
+import { render, screen } from "@testing-library/react"
+import App from "../components/App"
+
+describe("Homepage Rendering", () => {
+    it("should render the main heading", () => {
+        render(<App />)
+
+        // Replace 'Vite + React' with text actually present on your homepage
+        const heading = screen.getByText(/Hello World/i)
+        expect(heading).toBeInTheDocument()
+    })
+})
