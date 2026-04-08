2381678abbe05bb91801f757de0a033bfee4f6f1
Oliver Ward <oliver.ward94@gmail.com>
2026-04-08 15:56:00 +1200

    chore: update database schema

--- apps/api/src/config/db/schema.ts
+++ apps/api/src/config/db/schema.ts
@@ -2,6 +2,6 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const users = sqliteTable("users", {
     id: integer("id").primaryKey(),
-    name: text("name").notNull(),
-    age: integer("age"),
+    email: text("email").notNull(),
+    password: text("password").notNull(),
 })
