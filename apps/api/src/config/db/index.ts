a9e9e051231fab0a1e4237406801c7cb6cab3f12
Oliver Ward <oliver.ward94@gmail.com>
2026-04-08 16:00:21 +1200

    chore: add logger & update readme

--- apps/api/src/config/db/index.ts
+++ apps/api/src/config/db/index.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3"
 import { drizzle } from "drizzle-orm/better-sqlite3"
+import { logger } from "src/config/logger"
 
-import { logger } from "../logger"
 import * as schema from "./schema"
 
 const sqlite: Database.Database = new Database("db.sqlite")
